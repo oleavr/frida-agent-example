ab4551fb6f856bd0dacd1685c89577858ed4329a
Daniel Kalinowski <max0980@gmail.com>
2018-07-09 13:46:14 +0200

    Reverted indentations changes

--- agent/logger.ts
+++ agent/logger.ts
@@ -1,21 +1,21 @@
 
 enum LogPriority {
-  Verbose = 2,
-  Debug = 3,
-  Info = 4,
-  Warn = 5,
-  Error = 6,
-  Fatal = 7
+    Verbose = 2,
+    Debug = 3,
+    Info = 4,
+    Warn = 5,
+    Error = 6,
+    Fatal = 7
 };
 
 const androidLogWrite: any = new NativeFunction(
-  Module.findExportByName("liblog.so", "__android_log_write"),
-  "int",
-  ["int", "pointer", "pointer"]);
+    Module.findExportByName("liblog.so", "__android_log_write"),
+    "int",
+    ["int", "pointer", "pointer"]);
 
 const logTagBuf = Memory.allocUtf8String("frida");
 
 export function log(message: string): void {
-  const messageBuf = Memory.allocUtf8String(message);
-  androidLogWrite(LogPriority.Info, logTagBuf, messageBuf);
+    const messageBuf = Memory.allocUtf8String(message);
+    androidLogWrite(LogPriority.Info, logTagBuf, messageBuf);
 }
