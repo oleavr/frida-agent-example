63e58dc277b6fa900c7605ebff45846cb13e085d
Ole André Vadla Ravnås <oleavr@gmail.com>
2018-09-30 03:31:46 +0200

    Simplify agent to just logging “Hello World”
    Only showing how to split logic across multiple files.

--- agent/index.ts
+++ agent/index.ts
@@ -1,32 +1,3 @@
 import { log } from "./logger";
 
-rpc.exports = {
-    async init(stage: string, parameters: any): Promise<void> {
-        try {
-            log("[init]");
-
-            log("Let's see");
-            const socket = await Socket.connect({
-                family: AddressFamily.IPv4,
-                host: "127.0.0.1",
-                port: 1337
-            });
-            log("Connected");
-            await socket.output.writeAll(encode("Hello"));
-            log("Bye");
-        } catch (e) {
-            log("Oops: " + e.stack);
-        }
-    },
-    dispose(): void {
-        console.log("[dispose]");
-    }
-};
-
-type DuktapeBuffer = any;
-declare const TextEncoder: any;
-const encoder = new TextEncoder();
-
-function encode(str: string): DuktapeBuffer {
-    return encoder.encode(str);
-}
+log("Hello World!");
--- agent/logger.ts
+++ agent/logger.ts
@@ -1,21 +1,3 @@
-
-enum LogPriority {
-    Verbose = 2,
-    Debug = 3,
-    Info = 4,
-    Warn = 5,
-    Error = 6,
-    Fatal = 7
-};
-
-const androidLogWrite: any = new NativeFunction(
-    Module.findExportByName("liblog.so", "__android_log_write"),
-    "int",
-    ["int", "pointer", "pointer"]);
-
-const logTagBuf = Memory.allocUtf8String("frida");
-
 export function log(message: string): void {
-    const messageBuf = Memory.allocUtf8String(message);
-    androidLogWrite(LogPriority.Info, logTagBuf, messageBuf);
+    console.log(message);
 }
