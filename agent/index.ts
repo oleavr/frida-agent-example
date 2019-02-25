dd5e59aef3e2f827dcdae7f114d26cdb696005a5
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-02-25 03:57:03 +0100

    Update to the new Frida 12.4 API

--- agent/index.ts
+++ agent/index.ts
@@ -1,3 +1,15 @@
 import { log } from "./logger";
 
-log("Hello World!");
+const header = Memory.alloc(16);
+header
+    .writeU32(0xdeadbeef).add(4)
+    .writeU32(0xd00ff00d).add(4)
+    .writeU64(uint64("0x1122334455667788"));
+log(hexdump(header.readByteArray(16) as ArrayBuffer, { ansi: true }));
+
+Process.getModuleByName("libSystem.B.dylib")
+    .enumerateExports()
+    .slice(0, 16)
+    .forEach((exp, index) => {
+        log(`export ${index}: ${exp.name}`);
+    });
